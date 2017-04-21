ec8f8fd0d049e00c329364baef140a4bbc487486
Vasya Makarchuk <vasyamakarchuk@outlook.com>
2017-04-22 02:41:55 +0300

    invoice history

---
+++ src/app/components/invoice-list.component.ts
@@ -0,0 +1,12 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'invoicelist-comp',
+  templateUrl: 'templates/invoice-list/index.html',
+  styleUrls: ['templates/invoice-list/css/master.css']
+})
+export class InvoiceListComponent implements OnInit {
+  constructor() {  }
+
+  ngOnInit() {}
+}
--- src/app/modules/app.module.ts
+++ src/app/modules/app.module.ts
@@ -6,10 +6,11 @@ import { HttpModule } from '@angular/http';
 import { AppComponent } from '../components/app.component';
 import { InvoiceComponent } from '../components/invoice.component';
 import { GoodsComponent } from '../components/goods.component';
+import { InvoiceListComponent } from '../components/invoice-list.component';
 
 @NgModule({
     declarations: [
-        AppComponent, InvoiceComponent, GoodsComponent
+        AppComponent, InvoiceComponent, GoodsComponent, InvoiceListComponent
     ],
     imports: [
         BrowserModule,
